06829e50b428ae22dda8b6f3471db5665fd3f89c
Piotr Pacuk <piotrek.pacuk@gmail.com>
2023-07-11 20:18:04 +0200

    Added
    MovieDetails css

--- src/Pages/MovieDetails/MovieDetails.jsx
+++ src/Pages/MovieDetails/MovieDetails.jsx
@@ -7,9 +7,11 @@ export const MovieDetails = () => {
   const [movieCard, setMovieCard] = useState({});
   const { movieId } = useParams();
   const imageUrl = `https://image.tmdb.org/t/p/original/${movieCard.poster_path}`;
-  console.log(imageUrl);
-  const {  original_title, vote_average, overview } = movieCard;
-console.log(movieCard);
+  // console.log(imageUrl);
+  const { original_title, vote_average, overview, release_date } = movieCard;
+  // console.log(movieCard);
+  const getDate = new Date(`${release_date}`)
+  const getYear = getDate.getFullYear()
   const getMovieDetailsById = async movieId => {
     try {
       const movieCard = await fetchMovieDetailsById(`${movieId}`);
@@ -25,14 +27,21 @@ console.log(movieCard);
   }, [movieId]);
 
   return (
-    <div>
-      <img className={css.cardImg} src={imageUrl} alt="" />
-      <div>
-        {original_title}
-        {vote_average}
-        {overview}
+    <div className={css.movieDetailsContainer}>
+      <img
+        className={css.cardImg}
+        src={`https://image.tmdb.org/t/p/original/${movieCard.poster_path}`}
+        alt=""
+      />
+      <ul className={css.description}>
+        <li>
+          <h2>{original_title} ({ getYear})</h2>
+        </li>
+        <li>User Score : {Math.round(vote_average*10)}%</li>
+        <h3>Overview</h3>
+        <li>{overview}</li>
         {/* {genres.map(e => e.name)} */}
-      </div>
+      </ul>
     </div>
   );
 };
